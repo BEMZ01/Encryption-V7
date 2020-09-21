a47b02646dfbdfa622dcd79c488d68265fd045d2
Ben Miller <ben.miller1@hotmail.co.uk>
2020-09-21 08:56:52 +0000

    Updated help file (main.py) to add info about the different commands.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import encrypto as enc
-print(enc.getkey("f"))
-print(enc.encrypt(input("Text to encrypt.\n>>>: ")))
-print(enc.decrypt("message.enc", "key.enc"))
-print(enc.generate_frame())
+print(enc.getkey("f")) # generate a random key, requires a string lenth of > 1
+print(enc.encrypt(input("Text to encrypt.\n>>>: "))) # encrypt some text, returns tuple (message, key) also writes to message.enc and key.enc
+print(enc.decrypt("message.enc", "key.enc")) # decrypt message using files. Returns plaintext.
+print(enc.generate_frame()) # generate a frame from the webcam
 print("Generated random frame from last 5 sec")
